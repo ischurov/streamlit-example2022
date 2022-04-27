6ba628e15398aa4f90fb2e4fbadf3c1ff1066ccb
Ilya V. Schurov <ilya@schurov.com>
2022-04-27 17:14:07 +0200

    replace streamlit line_chart with seaborn

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def print_hello(name="World"):
     st.write(f"## Hello, {name}!")
@@ -14,8 +16,10 @@ Let's play with functions
 
 a = st.slider("a")
 x = np.linspace(-6, 6, 500)
-df = pd.DataFrame(dict(y=np.sin(a * x)))
-st.line_chart(df)
+df = pd.DataFrame(dict(x=x, y=np.sin(a * x)))
+fig, ax = plt.subplots()
+sns.lineplot(data=df, x='x', y='y', ax=ax)
+st.pyplot(fig)
 
 # print("Module helloworld is loaded")
 
